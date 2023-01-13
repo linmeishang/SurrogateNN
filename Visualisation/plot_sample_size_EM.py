f2c2f371f1748830be765b48be10da08062773de
linmeishang <50619441+linmeishang@users.noreply.github.com>
2023-01-13 14:35:11 +0100

    updated the code of result visualization

--- Visualisation/plot_sample_size_EM.py
+++ Visualisation/plot_sample_size_EM.py
@@ -17,103 +17,126 @@ models = ["MLP", "ResNet", "LSTM", "BiLSTM", ]
 
 
 #%%
-fig = plt.figure(figsize=(16, 20))
+# Define the figure size
+fig = plt.figure(figsize=(13, 18))
 
-# gs = gridspec.GridSpec(3, 3, figure = fig)
-
+# Basic setting of the figure
 sns.set()
-sns.set(font_scale = 1.5)
-Palette= sns.color_palette("colorblind", n_colors=4)
-sns.set_palette(Palette)
+# sns.set(font_scale = 2.0)
+sns.set_theme(style="whitegrid")
+## If you want to use colors
+# Palette= sns.color_palette("colorblind", n_colors=4)
+# sns.set_palette(Palette)
 
-
+# plt.rcParams.update({'font.size': 20})
 plt.rcParams["font.family"] = "Arial"
 
+
+
 ax=plt.subplot2grid((3,2),(0,0))
-# ax = plt.subplot(gs[0, 0])
-# plt.subplot(2, 2, 1)
+
 df = pd.read_excel('results.xlsx', sheet_name = 'R2')
 
-for i in models: 
+markers = ["o", "v", "s", "D"]
+line_styles = ["--", ":", "-.", "-"]
+
+for i, j, k in zip(models, markers, line_styles): 
+    sns.set_theme(style="whitegrid")
+    ax = sns.lineplot(x="sample size", y= i, marker=j, markersize=8, data=df, label= i, color = "dimgrey", linestyle=k)
     
-    ax = sns.lineplot(x="sample size", y= i, data=df, label= i, marker="o")
-    
-ax.set_xlabel("Sample size", loc="right")
-ax.set_ylabel("$\mathregular{R}^\mathregular{2}$", loc="top")
+ax.set_xlabel("Size of training set", loc="right")
+ax.set_ylabel("$\mathregular{R}^\mathregular{2}$", loc="top", fontsize=14)
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
 plt.title('(a)', y=-0.17)
 plt.ylim(0, 1)
-# plt.legend(loc='lower right')
 
 
-
-# plt.subplot(2, 2, 2)
-df = pd.read_excel('demo.xlsx', sheet_name = 'APE')
-
-# ax = plt.subplot(gs[0,1])
 ax=plt.subplot2grid((3,2),(0,1))
 
-for i in models: 
-    ax = sns.lineplot(x="sample size", y= i, data=df, label= i, marker="o")
+df = pd.read_excel('results.xlsx', sheet_name = 'RMSE')
 
-ax.set_xlabel("Sample size", loc="right")
-ax.set_ylabel("$\mathregular{APE}_\mathregular{relationship}$", loc="top")
-ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0, decimals=0))
+for i, j, k in zip(models, markers, line_styles): 
+    sns.set_theme(style="whitegrid") 
+    ax = sns.lineplot(x="sample size", y= i, marker=j, markersize=8, data=df, label= i, color = "dimgrey", linestyle=k)
+    
+ax.set_xlabel("Size of training set", loc="right")
+ax.set_ylabel("$\mathregular{RMSE}$", loc="top", fontsize=14)
+# ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0, decimals=0))
 plt.title('(b)', y=-0.17)
-plt.ylim(-0.01, 0.43)
+plt.ylim(-0.01, 0.18)
 
-# plt.legend(loc='upper right')
+
 
 ax=plt.subplot2grid((3,2),(1,0))
-# ax = plt.subplot(gs[1,0])
-# plt.subplot(2, 2, 3)
-df = pd.read_excel('demo.xlsx', sheet_name = 'A1')
 
-for i in models: 
-    ax = sns.lineplot(x="sample size", y= i, data=df, label= i, marker="o")
-ax.set_xlabel("Sample size", loc="right")
-ax.set_ylabel("$\mathregular{Accuracy}_\mathregular{corner}$", loc="top")
+df = pd.read_excel('results.xlsx', sheet_name = 'APE')
+
+for i, j, k in zip(models, markers, line_styles): 
+    sns.set_theme(style="whitegrid") 
+    ax = sns.lineplot(x="sample size", y= i, marker=j, markersize=8, data=df, label= i, color = "dimgrey", linestyle=k)
+    
+ax.set_xlabel("Size of training set", loc="right")
+ax.set_ylabel("$\mathregular{APE}_\mathregular{relationship}$", loc="top", fontsize=14)
 ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0, decimals=0))
-
 plt.title('(c)', y=-0.17)
-plt.ylim(0, 1.1)
-# plt.legend(loc='lower right')
+plt.ylim(-0.01, 1.1)
+
 
 
 
 ax=plt.subplot2grid((3,2),(1,1))
-# ax = plt.subplot(gs[1,1])
-#plt.subplot(2, 2, 4)
 
-df = pd.read_excel('demo.xlsx', sheet_name = 'A3')
+df = pd.read_excel('results.xlsx', sheet_name = 'A1')
 
-for i in models: 
-    
-    ax = sns.lineplot(x="sample size", y= i, data=df, label= i, marker="o")
+for i, j, k in zip(models, markers, line_styles):
+    sns.set_theme(style="whitegrid")
+    ax = sns.lineplot(x="sample size", y= i, marker=j, markersize=8, data=df, label= i, color = "dimgrey", linestyle=k)
 
-ax.set_xlabel("Sample size", loc="right")
-ax.set_ylabel("$\mathregular{Accuracy}_\mathregular{constraint}$", loc="top")
+ax.set_xlabel("Size of training set", loc="right")
+ax.set_ylabel("$\mathregular{Accuracy}_\mathregular{corner}$", loc="top", fontsize=14)
 ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0, decimals=0))
+
 plt.title('(d)', y=-0.17)
+plt.ylim(0, 1.1)
+
+
+
+ax=plt.subplot2grid((3,2),(2,0))
+
+df = pd.read_excel('results.xlsx', sheet_name = 'A2')
+
+for i, j, k in zip(models, markers, line_styles):
+    sns.set_theme(style="whitegrid")    
+    ax = sns.lineplot(x="sample size", y= i, marker=j, markersize=8, data=df, label= i, color = "dimgrey", linestyle=k)
+
+ax.set_xlabel("Size of training set", loc="right")
+ax.set_ylabel("$\mathregular{Accuracy}_\mathregular{constraint}$", loc="top", fontsize=14)
+ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0, decimals=0))
+plt.title('(e)', y=-0.17)
 plt.ylim(0.8, 1.01)
 # plt.legend(loc='lower right')
 
 
-ax=plt.subplot2grid((3,8),(2,2),colspan=4)
+ax=plt.subplot2grid((3,2),(2,1))
+# ax=plt.subplot2grid((3,8),(2,0),colspan=4)
 # plt.subplot(3, 2, 6)
 #ax = plt.subplot(gs[2,0], )
 
-df = pd.read_excel('demo.xlsx', sheet_name = 'score')
+df = pd.read_excel('results.xlsx', sheet_name = 'score')
 
-for i in models: 
-    ax = sns.lineplot(x="sample size", y= i, data=df, label= i, marker="o")
+for i, j, k in zip(models, markers, line_styles):
+    sns.set_theme(style="whitegrid") 
+    ax = sns.lineplot(x="sample size", y= i, marker=j, markersize=8, data=df, label= i, color = "dimgrey", linestyle=k)
 
-ax.set_xlabel("Sample size", loc="right")
-ax.set_ylabel("Total score", loc="top")
+ax.set_xlabel("Size of training set", loc="right")
+ax.set_ylabel("Total score", loc="top", fontsize=14)
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
-plt.title('(e)', y=-0.17)
+plt.title('(f)', y=-0.17)
 plt.ylim(0, 3.2)
 
 
 plt.show()
-fig.savefig("New_plots_2.png",dpi=300)
+fig.savefig("Plots_EM_SampleSize.png",dpi=300)
+
+# %%
+
